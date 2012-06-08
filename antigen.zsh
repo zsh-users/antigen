a91386310e3f1a82be137bb3d78a82800c5ec0d7
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-06-08 19:05:17 +0530

    Add a todo note on bundle-apply.
    We can have a performance improvement at shell startup by loading completions in
    bundle-apply, only if any of the repositories changed.

--- antigen.zsh
+++ antigen.zsh
@@ -194,6 +194,8 @@ bundle-theme () {
 
 bundle-apply () {
     # Initialize completion.
+    # TODO: Only load completions if there are any changes to the bundle
+    # repositories.
     compinit -i
 }
 
