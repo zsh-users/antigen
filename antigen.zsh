3c5ff1c1e2d09d35963ca574b4dd57e7e6a2b7b2
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-06-09 20:46:10 +0530

    Add antigen-bundles command.
    This command lets you bulk-add bundles with one command, and piping bundle
    specs, one on each line into this command. Best used with heredoc syntax.

--- antigen.zsh
+++ antigen.zsh
@@ -56,6 +56,20 @@ antigen-bundle () {
 
 }
 
+antigen-bundles () {
+    # Bulk add many bundles at one go. Empty lines and lines starting with a `#`
+    # are ignored. Everything else is given to `antigen-bundle` as is, no
+    # quoting rules applied.
+
+    local line
+
+    grep -v '^$\|^#' | while read line; do
+        # Using `eval` so that we can use the shell-style quoting in each line
+        # piped to `antigen-bundles`.
+        eval "antigen-bundle $line"
+    done
+}
+
 antigen-bundle-update () {
     # Update your bundles, i.e., `git pull` in all the plugin repos.
     -antigen-echo-record | awk '{print $1}' | sort -u | while read url; do
