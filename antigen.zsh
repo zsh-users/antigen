f783f38601964d2b9142a96a9c444004bbe817f8
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-08-10 22:18:01 +0530

    Comments and indentation.

--- antigen.zsh
+++ antigen.zsh
@@ -204,9 +204,9 @@ antigen-update () {
     if [[ $url == *\|* ]]; then
         local current_branch=${$(--plugin-git symbolic-ref HEAD)##refs/heads/}
         local requested_branch="${url#*|}"
-        # Only do the checkout when we are not already on the branch
+        # Only do the checkout when we are not already on the branch.
         [[ $requested_branch != $current_branch ]] &&
-        --plugin-git checkout $requested_branch
+            --plugin-git checkout $requested_branch
     fi
 
     if ! [[ -z $old_rev || $old_rev == $new_rev ]]; then
