a54264963745656e6d4d7b71847af3f4c47360e6
Shrikant Sharat <shrikantsharat.k@gmail.com>
2013-02-21 23:05:57 +0530

    Refactorings on the selfupdate function.

--- antigen.zsh
+++ antigen.zsh
@@ -292,12 +292,14 @@ antigen-revert () {
 
 }
 
+# Update (with `git pull`) antigen itself.
+# TODO: Once update is finished, show a summary of the new commits, as a kind of
+# "what's new" message.
 antigen-selfupdate () {
-    (
-        cd $_ANTIGEN_INSTALL_DIR
-        # Sanity checks
-        if [ ! -d .git ]; then
-            echo "antigen is not under git CVS"
+    ( cd $_ANTIGEN_INSTALL_DIR
+        if [[ ! -d .git ]]; then
+            echo "Your copy of antigen doesn't appear to be a git clone. " \
+                "The 'selfupdate' command cannot work in this case."
             return 1
         fi
         git pull
