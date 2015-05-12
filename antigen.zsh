7860ce7aecdbed8fd8ff75472ac59c52c2ac9a7e
Shrikant Sharat <shrikantsharat.k@gmail.com>
2015-05-12 15:44:50 +0530

    Fix multi-line message from revert command.

--- antigen.zsh
+++ antigen.zsh
@@ -143,8 +143,8 @@ antigen-revert () {
                 checkout "$(echo "$line" | cut -d: -f2)" 2> /dev/null
         done
 
-        echo "Reverted to state before running -update on
-                $(cat $ADOTDIR/revert-info | sed -n '1p')."
+        echo "Reverted to state before running -update on $(
+                cat $ADOTDIR/revert-info | sed -n '1p')."
 
     else
         echo 'No revert information available. Cannot revert.' >&2
