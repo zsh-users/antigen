89c43ca46ab10ec4bbeb086abfadc3c61d23d9b5
DC* <des@riseup.net>
2016-10-15 17:18:34 -0300

    Add check for 4.3.7 and below

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -665,11 +665,16 @@ antigen-snapshot () {
 }
 antigen-theme () {
     # This is only needed on interactive mode
-    autoload -U add-zsh-hook
+    autoload -U add-zsh-hook is-at-least
     local hook
     for hook in chpwd precmd preexec periodic; do
-        add-zsh-hook -D "${hook}" "prompt_*"
-        add-zsh-hook -D "${hook}" "*_${hook}" # common in omz themes 
+        # add-zsh-hook's -D option was introduced first in 4.3.6-dev and
+        # 4.3.7 first stable, 4.3.5 and below may experiment minor issues
+        # while switching themes interactively.
+        if is-at-least 4.3.7; then
+            add-zsh-hook -D "${hook}" "prompt_*"
+            add-zsh-hook -D "${hook}" "*_${hook}" # common in omz themes 
+        fi
         add-zsh-hook -d "${hook}" "vcs_info"  # common in omz themes
     done
 
