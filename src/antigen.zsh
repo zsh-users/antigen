1899045a192335d523a145700e957e12a9acf476
DC* <syncr@riseup.net>
2016-10-25 23:45:16 +0200

    Antigen fast boot method for bundle loading (#296)
    * Antigen fast boot method for bundle loading
    
    - Load straight from cache
    - Compiles compinit
    - Hooks antigen functions
    
    Reduced startup time from ~0.30s to ~0.15s
    
    Added _ANTIGEN_FAST_BOOT_ENABLED env variable to enable or disable
    this functionallity.
    
    Bugfix/completions on fastboot (#300)
    
    * Disabled antigen own completions before lazyloading
    * Fix lazyload antigen
    * Update tests for cache payload

--- src/antigen.zsh
+++ src/antigen.zsh
@@ -3,23 +3,29 @@
 #          and Contributors <https://github.com/zsh-users/antigen/contributors>
 # Homepage: http://antigen.sharats.me
 # License: MIT License <mitl.sharats.me>
+autoload -U is-at-least;
 
 # Each line in this string has the following entries separated by a space
 # character.
 # <repo-url>, <plugin-location>, <bundle-type>, <has-local-clone>
-local _ANTIGEN_BUNDLE_RECORD=""
-local _ANTIGEN_INSTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
+local _ANTIGEN_BUNDLE_RECORD=${_ANTIGEN_BUNDLE_RECORD:-""}
+if is-at-least 4.3.7; then
+  local _ANTIGEN_INSTALL_DIR=${0:A:h}
+else
+  local _ANTIGEN_INSTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
+fi
 local _ANTIGEN_CACHE_ENABLED=${_ANTIGEN_CACHE_ENABLED:-true}
 local _ANTIGEN_COMP_ENABLED=${_ANTIGEN_COMP_ENABLED:-true}
 local _ANTIGEN_INTERACTIVE=${_ANTIGEN_INTERACTIVE_MODE:-false}
 local _ANTIGEN_RESET_THEME_HOOKS=${_ANTIGEN_RESET_THEME_HOOKS:-true}
 local _ANTIGEN_AUTODETECT_CONFIG_CHANGES=${_ANTIGEN_AUTODETECT_CONFIG_CHANGES:-true}
 local _ANTIGEN_FORCE_RESET_COMPDUMP=${_ANTIGEN_FORCE_RESET_COMPDUMP:-true}
+local _ANTIGEN_FAST_BOOT_ENABLED=${_ANTIGEN_FAST_BOOT_ENABLED:-true}
 
 # Do not load anything if git is not available.
-if ! which git &> /dev/null; then
-  echo 'Antigen: Please install git to use Antigen.' >&2
-  return 1
+if (( ! $+commands[git] )); then
+    echo 'Antigen: Please install git to use Antigen.' >&2
+    return 1
 fi
 
 # Used to defer compinit/compdef
@@ -36,9 +42,9 @@ antigen () {
   fi
   shift
 
-  if functions "antigen-$cmd" > /dev/null; then
-    "antigen-$cmd" "$@"
-  else
-    echo "Antigen: Unknown command: $cmd" >&2
-  fi
+    if (( $+functions[antigen-$cmd] )); then
+        "antigen-$cmd" "$@"
+    else
+        echo "Antigen: Unknown command: $cmd" >&2
+    fi
 }
---
+++ src/boot.zsh
@@ -0,0 +1,63 @@
+# Used for lazy-loading.
+_ANTIGEN_SOURCE="$_ANTIGEN_INSTALL_DIR/antigen.zsh"
+# Used to fastboot antigen
+_ZCACHE_PAYLOAD="${ADOTDIR:-$HOME/.antigen}/.cache/.zcache-payload"
+
+# Use this functionallity only if both CACHE and FASTBOOT options are enabled.
+if [[ $_ANTIGEN_CACHE_ENABLED == true && $_ANTIGEN_FAST_BOOT_ENABLED == true ]]; then
+
+    # If there is cache (zcache payload), and it wasn't loaded then procced.
+
+    # The condition "$_ZCACHE_CACHE_LOADED != true" was crafted this way because
+    # $_ZCACHE_CACHE_LOADED variable is otherwise undefined, so it seems easier to
+    # check for a known value.
+    if [[ $_ZCACHE_CACHE_LOADED != true && -f "$_ZCACHE_PAYLOAD" ]]; then
+
+        # Do load zcache payload, this has the following effects:
+        #   - _ANTIGEN_BUNDLE_RECORD is updated from cache
+        #   - _ZCACHE_CACHE_LOADED is set to TRUE
+        #   - _antigen is updated from cache
+        #   - fpath is updated from cache
+        #   - autoload compinit && compinit -id $ANTIGEN_COMPDUMPFILE
+        source "$_ZCACHE_PAYLOAD"
+
+        # Lazyload wrapper
+        -antigen-lazyloader () {
+            # Hook antigen functions to lazy load antigen itself
+            for command in ${(Mok)functions:#antigen*}; do
+                # Once any of the hooked functions are called and antigen is finally
+                # loaded what will happen is that antigen overwrittes the hooked functions
+                # so no other call to them will be executed, thus no need to
+                # 'unhook' or uninitialize them.
+                eval "$command () { source "$_ANTIGEN_SOURCE"; eval $command \$@ }"
+            done
+            unfunction -- '-antigen-lazyloader'
+        }
+
+        # Disable antigen commands
+        _commands=('use' 'bundle' 'bundles' 'theme' 'list' 'apply' 'cleanup' \
+         'help' 'list' 'reset' 'restore' 'revert' 'snapshot' 'selfupdate' 'update' 'version')
+        for command in $_commands; do
+            eval "antigen-$command () {}"
+        done
+
+        # On antigen apply or init
+        antigen () {
+            if [[ "$1" == "apply" || "$1" == "init" ]]; then
+                -antigen-lazyloader
+            fi
+        }
+
+        # On antigen-apply
+        antigen-apply () {
+            -antigen-lazyloader
+        }
+
+        # On antigen-init
+        antigen-init () {
+            -antigen-lazyloader
+        }
+
+        return
+    fi
+fi
