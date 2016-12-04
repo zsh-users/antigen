57fc852475388ff1dd1b029bb109c40493a11c07
DC* <syncr@riseup.net>
2016-12-04 09:14:49 +0200

    `antigen update` command handle single bundle (#326)
    * Add test for `antigen-update` command with single plugin
    * `antigen-update` handles revert-info and bundles with branches

---
+++ src/lib/revert-info.zsh
@@ -0,0 +1,23 @@
+# Updates revert-info data with git hash.
+#
+# This does process only cloned bundles.
+#
+# Usage
+#    -antigen-revert-info
+#
+# Returns
+#    Nothing. Generates/updates $ADOTDIR/revert-info.
+-antigen-revert-info() {
+  # Update your bundles, i.e., `git pull` in all the plugin repos.
+  date >! $ADOTDIR/revert-info
+
+  -antigen-get-cloned-bundles | while read url; do
+    local clone_dir="$(-antigen-get-clone-dir "$url")"
+    if [[ -d "$clone_dir" ]]; then
+      (echo -n "$clone_dir:"
+        cd "$clone_dir"
+        git rev-parse HEAD) >> $ADOTDIR/revert-info
+    fi
+  done
+}
+
