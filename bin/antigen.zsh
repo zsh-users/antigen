e2ff90dc94df14df3eae9cfd70ff8c32f36ac9cc
DC* <des@riseup.net>
2016-12-09 00:18:26 -0300

    Using default library constant

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -135,12 +135,11 @@ antigen () {
 # Returns
 #   List of themes by name
 -antigen-get-themes () {
-  local library="robbyrussell/oh-my-zsh"
+  local library='robbyrussell/oh-my-zsh'
   local bundle=$(-antigen-find-bundle $library)
 
   if [[ -n "$bundle" ]]; then
-    local url=$(-antigen-resolve-bundle-url $bundle)
-    local dir=$(-antigen-get-clone-dir $url)
+    local dir=$(-antigen-get-clone-dir $ANTIGEN_DEFAULT_REPO_URL)
     echo $(ls $dir/themes | sed 's/.zsh-theme//')
   fi
   
