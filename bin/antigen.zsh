411302139dfd3dcaf4745136a86bf341031bcb0d
DC* <des@riseup.net>
2016-12-10 02:15:14 -0300

    Build release v1.3.0 (#349)

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -962,7 +962,7 @@ antigen-use () {
     fi
 }
 antigen-version () {
-    echo "Antigen v1.2.4"
+    echo "Antigen v1.3.0"
 }
 #compdef _antigen
 # Setup antigen's autocompletion
@@ -1108,7 +1108,7 @@ _antigen () {
 
     _payload+="#-- START ZCACHE GENERATED FILE\NL"
     _payload+="#-- GENERATED: $(date)\NL"
-    _payload+='#-- ANTIGEN v1.2.4\NL'
+    _payload+='#-- ANTIGEN v1.3.0\NL'
     for bundle in $_ZCACHE_BUNDLES; do
         # -antigen-load-list "$url" "$loc" "$make_local_clone"
         eval "$(-antigen-parse-bundle ${=bundle})"
@@ -1142,7 +1142,7 @@ _antigen () {
     # \NL (\n) prefix is for backward compatibility
     _payload+="export _ANTIGEN_BUNDLE_RECORD=\"\NL${(j:\NL:)_bundles_meta}\"\NL"
     _payload+="export _ZCACHE_CACHE_LOADED=true\NL"
-    _payload+="export _ZCACHE_CACHE_VERSION=v1.2.4\NL"
+    _payload+="export _ZCACHE_CACHE_VERSION=v1.3.0\NL"
     _payload+="#-- END ZCACHE GENERATED FILE\NL"
 
     echo -E $_payload | sed 's/\\NL/\'$'\n/g' >! "$_ZCACHE_PAYLOAD_PATH"
