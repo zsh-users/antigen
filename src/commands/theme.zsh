597880990a11e04bc4f2791366f5ea3121c4c9b4
DC* <des@riseup.net>
2016-12-27 06:52:07 +0200

    Interactively changing between themes (#368)
    * Fix interactively changing between themes
    * Add tests and handle corner cases

--- src/commands/theme.zsh
+++ src/commands/theme.zsh
@@ -1,37 +1,63 @@
+# Loads a given theme.
+#
+# Shares the same syntax as antigen-bundle command.
+#
+# Usage
+#   antigen-theme zsh/theme[.zsh-theme]
+#
+# Returns
+#   0 if everything was succesfully
 antigen-theme () {
-    if [[ $_ANTIGEN_RESET_THEME_HOOKS == true ]]; then
-        -antigen-theme-reset-hooks
-    fi
+  local record
+  local result=0
 
-    if [[ "$1" != */* && "$1" != --* ]]; then
-        # The first argument is just a name of the plugin, to be picked up from
-        # the default repo.
-        local name="${1:-robbyrussell}"
-        antigen-bundle --loc=themes/$name --btype=theme
+  if [[ $_ANTIGEN_RESET_THEME_HOOKS == true ]]; then
+      -antigen-theme-reset-hooks
+  fi
 
-    else
-        antigen-bundle "$@" --btype=theme
+  record=$(-antigen-find-record "theme")
 
-    fi
+  if [[ "$1" != */* && "$1" != --* ]]; then
+    # The first argument is just a name of the plugin, to be picked up from
+    # the default repo.
+    local name="${1:-robbyrussell}"
+    antigen-bundle --loc=themes/$name --btype=theme
+
+  else
+    antigen-bundle "$@" --btype=theme
+
+  fi
+  result=$?
+
+  # Remove a theme from the record if the following conditions apply:
+  #   - there was no error in bundling the given theme
+  #   - there is a theme registered
+  #   - registered theme is not the same as the current one
+  if [[ $result == 0 && -n $record && ! $record =~ "$@" ]]; then
+    # Remove entire line plus $\n character
+    _ANTIGEN_BUNDLE_RECORD=${_ANTIGEN_BUNDLE_RECORD//$'\n'$record/}
+  fi
+
+  return $result
 }
 
 -antigen-theme-reset-hooks () {
-    # This is only needed on interactive mode
-    autoload -U add-zsh-hook is-at-least
-    local hook
-    
-    # Clear out prompts
-    PROMPT=""
-    RPROMPT=""
-    
-    for hook in chpwd precmd preexec periodic; do
-        # add-zsh-hook's -D option was introduced first in 4.3.6-dev and
-        # 4.3.7 first stable, 4.3.5 and below may experiment minor issues
-        # while switching themes interactively.
-        if is-at-least 4.3.7; then
-            add-zsh-hook -D "${hook}" "prompt_*"
-            add-zsh-hook -D "${hook}" "*_${hook}" # common in omz themes 
-        fi
-        add-zsh-hook -d "${hook}" "vcs_info"  # common in omz themes
-    done
+  # This is only needed on interactive mode
+  autoload -U add-zsh-hook is-at-least
+  local hook
+
+  # Clear out prompts
+  PROMPT=""
+  RPROMPT=""
+
+  for hook in chpwd precmd preexec periodic; do
+    # add-zsh-hook's -D option was introduced first in 4.3.6-dev and
+    # 4.3.7 first stable, 4.3.5 and below may experiment minor issues
+    # while switching themes interactively.
+    if is-at-least 4.3.7; then
+      add-zsh-hook -D "${hook}" "prompt_*"
+      add-zsh-hook -D "${hook}" "*_${hook}" # common in omz themes 
+    fi
+    add-zsh-hook -d "${hook}" "vcs_info"  # common in omz themes
+  done
 }
