9f1ffe673b1426c2c2be39a2f234025e69cb010a
DC* <des@riseup.net>
2017-03-02 01:00:41 -0300

    Remove deprecated commands and update README.md

--- src/commands/lib.zsh
+++
@@ -1,6 +0,0 @@
-# For backwards compatibility.
-antigen-lib () {
-  -antigen-use-oh-my-zsh
-  echo '`antigen-lib` is deprecated and will soon be removed.'
-  echo 'Use `antigen-use oh-my-zsh` instead.'
-}
