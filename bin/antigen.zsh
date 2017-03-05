63dc40659ee8e1d3d5a975b7d27b206c77fed798
DC* <des@riseup.net>
2017-03-05 02:35:23 -0300

    Add config modification check for antigen boot

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -53,6 +53,46 @@ _ANTIGEN_SOURCE="$_ANTIGEN_INSTALL_DIR/antigen.zsh"
 # Used to fastboot antigen
 _ZCACHE_PAYLOAD="${ADOTDIR:-$HOME/.antigen}/.cache/.zcache-payload"
 
+# Enable or disable timestamp checks
+_ZCACHE_TIMESTAMP_CHECK_ENABLED=${_ZCACHE_TIMESTAMP_CHECK_ENABLED:-true}
+_ZCACHE_TIMESTAMP_CHECK=($HOME/.zshrc ${ADOTDIR:-$HOME}/.antigenrc)
+
+# Used to do full boostrap
+_ZCACHE_TIMESTAMP="${ADOTDIR:-$HOME/.antigen}/.timestamp"
+
+if [[ $_ZCACHE_TIMESTAMP_CHECK_ENABLED == true ]]; then
+  # source: http://stackoverflow.com/q/17878684
+  if stat -c %Y . >/dev/null 2>&1; then
+    -antigen-get-timestamp() { stat -c %Y "$1" 2>/dev/null; }
+  elif stat -f %m . >/dev/null 2>&1; then
+    -antigen-get-timestamp() { stat -f %m "$1" 2>/dev/null; }
+  elif date -r . +%s >/dev/null 2>&1; then
+    -antigen-get-timestamp() { stat -r "$1" +%s 2>/dev/null; }
+  else
+    echo '-antigen-get-timestamp() is unsupported' >&2
+    -antigen-get-timestamp() { printf '%s' 0; }
+  fi
+
+  local timestamp=0
+  for config in $_ZCACHE_TIMESTAMP_CHECK; do
+    config_timestamp=$(-antigen-get-timestamp $config)
+    if [ $timestamp -lt "$config_timestamp" ]; then
+      timestamp=$config_timestamp
+    fi
+  done
+
+  if [ -r $_ZCACHE_TIMESTAMP ]; then
+    saved=$(cat $_ZCACHE_TIMESTAMP)
+    if [ $saved -lt $timestamp ]; then
+      # Do full bootstrap
+      echo $timestamp > $_ZCACHE_TIMESTAMP
+      _ANTIGEN_FAST_BOOT_ENABLED=false
+    fi
+  else
+    echo $timestamp > $_ZCACHE_TIMESTAMP
+  fi
+fi
+
 # Use this functionallity only if both CACHE and FASTBOOT options are enabled.
 if [[ $_ANTIGEN_CACHE_ENABLED == true && $_ANTIGEN_FAST_BOOT_ENABLED == true ]]; then
 
