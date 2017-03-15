0143a4e18f287e62c4beb5fc1d7416f564b752e3
DC* <syncr@riseup.net>
2017-03-15 14:55:29 -0300

    Apply logic to work on zsh <= 4.3.7

--- antigen.zsh
+++ antigen.zsh
@@ -1,3 +1,8 @@
-_ANTIGEN_INSTALL_DIR=${0:A:h}
+autoload -U is-at-least;
+if is-at-least 4.3.7; then
+  _ANTIGEN_INSTALL_DIR=${0:A:h}
+else
+  _ANTIGEN_INSTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
+fi
 source $_ANTIGEN_INSTALL_DIR/bin/antigen.zsh
 
