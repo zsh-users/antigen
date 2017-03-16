483a7ad256fc6ad4bd36a990df2dc1048b995998
DC* <des@riseup.net>
2017-03-15 21:47:39 -0300

    Properly set _ANTIGEN_INSTALL_DIR (#442)
    * Adapted structure to properly handle _ANTIGEN_INSTALL_DIR
    * Apply logic to work on zsh <= 4.3.7

--- antigen.zsh
+++ antigen.zsh
@@ -1,1 +1,8 @@
-bin/antigen.zsh
+autoload -U is-at-least;
+if is-at-least 4.3.7; then
+  _ANTIGEN_INSTALL_DIR=${0:A:h}
+else
+  _ANTIGEN_INSTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
+fi
+source $_ANTIGEN_INSTALL_DIR/bin/antigen.zsh
+
