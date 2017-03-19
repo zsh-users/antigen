08aaaf54cab4037511167f79259e5bfa4271ad65
DC* <des@riseup.net>
2017-03-19 14:09:49 -0300

    Removed unneeded set env variable for stats generation

--- tests/stats.sh
+++ tests/stats.sh
@@ -3,7 +3,7 @@ PROJECT=${1:-$HOME}
 SHELL=${2:-zsh}
 ZSHRC=$HOME/.zshrc
 TMP=${ZSHRC}.stats-tmp
-CMD="_ZCACHE_CACHE_LOADED=false $SHELL -ic exit"
+CMD="$SHELL -ic exit"
 MTIME=/tmp/mtime
 
 # Back up configuration
@@ -17,7 +17,7 @@ for x in $(seq 1 20); do
     tail -1 $MTIME
 done
 
-awk '{ total += $11; user += $5; sys += $7; count++ } END {  printf "\nAverage:\ntotal %.3fs user %.3fs sys %.3fs\n", total/count, user/count, sys/count }' $MTIME
+awk '{ total += $10; user += $4; sys += $6; count++ } END {  printf "\nAverage:\ntotal %.3fs user %.3fs sys %.3fs\n", total/count, user/count, sys/count }' $MTIME
 
 rm -f $MTIME
 cp $TMP $ZSHRC
