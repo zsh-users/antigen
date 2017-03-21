8706f8bd25aa086d563feb0e8a9df1d087bd39f5
DC* <des@riseup.net>
2017-03-21 01:39:34 -0300

    Fix cache generation

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -52,7 +52,7 @@ fi
 # Each line in this string has the following entries separated by a space
 # character.
 # <repo-url>, <plugin-location>, <bundle-type>, <has-local-clone>
-typeset -aU _ANTIGEN_BUNDLE_RECORD
+[[ -z _ANTIGEN_BUNDLE_RECORD ]] && typeset -aU _ANTIGEN_BUNDLE_RECORD
 
 # Do not load anything if git is not available.
 if (( ! $+commands[git] )); then
@@ -847,7 +847,7 @@ autoload -Uz add-zsh-hook; add-zsh-hook precmd _antigen_compinit
 compdef () {}\NL"
 
   _payload+=$_sources
-  _payload+="typeset -aU _ANTIGEN_BUNDLE_RECORD=("${(qq)_ANTIGEN_BUNDLE_RECORD}")\NL"
+  _payload+="typeset -aU _ANTIGEN_BUNDLE_RECORD; _ANTIGEN_BUNDLE_RECORD=("$(print ${(qq)_ANTIGEN_BUNDLE_RECORD})")\NL"
   _payload+="_ANTIGEN_CACHE_LOADED=true _ANTIGEN_CACHE_VERSION=v1.4.1\NL"
 
   # Cache omz/prezto env variables. See https://github.com/zsh-users/antigen/pull/387
