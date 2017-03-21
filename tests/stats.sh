8733af015ea4bb2eec6ed96c68c40464b721660b
DC* <des@riseup.net>
2017-03-21 15:22:24 -0300

    `antigen-bundles` command properly handle comments (#451)
    - Adapted stats generation script
    - Updated `.antigenrc` configuration

--- tests/stats.sh
+++ tests/stats.sh
@@ -9,15 +9,29 @@ MTIME=/tmp/mtime
 # Back up configuration
 cp $ZSHRC $TMP 
 
+echo Installing bundles...
+eval "$SHELL -ic 'rm $HOME/.antigen/init.zsh'"
+eval "$SHELL -ic 'rm -rf $HOME/.antigen/repos/'"
+
 cp $PROJECT/tests/.zshrc $HOME/.zshrc
 eval $CMD
+echo Done.
+echo
 
+echo Installed bundles:
+eval "$SHELL -ic 'source $PROJECT/antigen.zsh; antigen list'"
+echo
+
+echo Performance testing...
 for x in $(seq 1 20); do
     (eval time $CMD) &>> $MTIME
     tail -1 $MTIME
 done
 
+echo
 awk '{ total += $10; user += $4; sys += $6; count++ } END {  printf "\nAverage:\ntotal %.3fs user %.3fs sys %.3fs\n", total/count, user/count, sys/count }' $MTIME
+
+echo Done.
 
 rm -f $MTIME
 cp $TMP $ZSHRC
