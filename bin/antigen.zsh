88118173dfe993275669cd8dc0a161d6d2aec4c7
DC* <des@riseup.net>
2017-04-02 01:15:53 -0300

    Add logic to save VERSION branch/tag - git hash

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -801,7 +801,7 @@ _ZCACHE_BUNDLE=${_ZCACHE_BUNDLE:-false}
 
   _payload="#-- START ZCACHE GENERATED FILE
 #-- GENERATED: $(date)
-#-- ANTIGEN v1.4.1
+#-- ANTIGEN develop (aca8eef)
 $(functions -- _antigen)
 antigen () {
   [[ \"\$ZSH_EVAL_CONTEXT\" =~ \"toplevel:*\" || \"\$ZSH_EVAL_CONTEXT\" =~ \"cmdarg:*\" ]] && \
@@ -819,7 +819,7 @@ compdef () {}\NL"
   _payload+=$_sources
   _payload+="typeset -aU _ANTIGEN_BUNDLE_RECORD;\
       _ANTIGEN_BUNDLE_RECORD=("$(print ${(qq)_ANTIGEN_BUNDLE_RECORD})")\NL"
-  _payload+="_ANTIGEN_CACHE_LOADED=true _ANTIGEN_CACHE_VERSION=v1.4.1\NL"
+  _payload+="_ANTIGEN_CACHE_LOADED=true _ANTIGEN_CACHE_VERSION=develop (aca8eef)\NL"
 
   # Cache omz/prezto env variables. See https://github.com/zsh-users/antigen/pull/387
   if [[ -n "$ZSH" ]]; then
@@ -1413,7 +1413,7 @@ antigen-use () {
 }
 
 antigen-version () {
-  echo "Antigen v1.4.1"
+  echo "Antigen develop (aca8eef)"
 }
 
 #compdef _antigen
