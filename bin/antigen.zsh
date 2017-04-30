d4890a837098a57f2f8e70c3e4588e19638a6016
DC* <des@riseup.net>
2017-04-29 22:49:13 -0300

    Remove compdump on antigen-reset

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -1160,6 +1160,7 @@ antigen-purge () {
 antigen-reset () {
   [[ -f "$ANTIGEN_CACHE" ]] && rm -f "$ANTIGEN_CACHE"
   [[ -f "$ANTIGEN_RSRC" ]] && rm -f "$ANTIGEN_RSRC"
+  [[ -f "$ANTIGEN_COMPDUMP" ]] && rm -f "$ANTIGEN_COMPDUMP"
   echo 'Done. Please open a new shell to see the changes.'
 }
 antigen-restore () {
