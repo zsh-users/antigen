4871058be32eee977df3dee24f6873fdd4479fa5
DC* <des@riseup.net>
2017-06-16 01:17:13 -0300

    Remove sourcing antigen from wrapper function

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -1791,8 +1791,6 @@ antigen () {
 typeset -gaU fpath path
 fpath+=(${_fpath[@]}) path+=(${_PATH[@]})
 _antigen_compinit () {
-  source "$_ANTIGEN_INSTALL_DIR/antigen.zsh"
-  TRACE "Gonna load compdump file @ _antigen_compinit" COMPDUMP
   autoload -Uz compinit; compinit -d "$ANTIGEN_COMPDUMP"; compdef _antigen antigen
   add-zsh-hook -D precmd _antigen_compinit
 }
