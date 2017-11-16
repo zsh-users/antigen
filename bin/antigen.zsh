ad25123534dba795c7030ce92bc4cf7dc121381a
DC* <des@riseup.net>
2017-11-16 13:02:50 -0300

    Avoid adding repeated entries to antigen rsrc (#606)

--- bin/antigen.zsh
+++ bin/antigen.zsh
@@ -1864,7 +1864,7 @@ EOC
     # There always should be 5 steps from original source as the correct way is to use
     # `antigen` wrapper not `antigen-apply` directly and it's called by an extension.
     LOG "TRACE: ${funcfiletrace}"
-    if [[ $ANTIGEN_AUTO_CONFIG == true ]]; then
+    if [[ $ANTIGEN_AUTO_CONFIG == true && $#ANTIGEN_CHECK_FILES -eq 0 ]]; then
       ANTIGEN_CHECK_FILES+=(~/.zshrc)
       if [[ $#funcfiletrace -ge 6 ]]; then
         ANTIGEN_CHECK_FILES+=("${${funcfiletrace[6]%:*}##* }")
