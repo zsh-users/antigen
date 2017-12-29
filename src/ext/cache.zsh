7bce2ccc2d836fcd3ab19a5a0cb908826d9f38f3
DC* <des@riseup.net>
2017-12-29 00:26:55 -0300

    Fix missing common resource file (#635)

--- src/ext/cache.zsh
+++ src/ext/cache.zsh
@@ -126,7 +126,11 @@ EOC
     # `antigen` wrapper not `antigen-apply` directly and it's called by an extension.
     LOG "TRACE: ${funcfiletrace}"
     if [[ $ANTIGEN_AUTO_CONFIG == true && $#ANTIGEN_CHECK_FILES -eq 0 ]]; then
-      ANTIGEN_CHECK_FILES+=(~/.zshrc)
+      # Check common configuration file does exist.
+      if [[ -f $HOME/.zshrc ]]; then
+        ANTIGEN_CHECK_FILES+=($HOME/.zshrc)
+      fi
+      # TODO Fix: Fuzzy match shoud be replaced by a sane way to determine it.
       if [[ $#funcfiletrace -ge 6 ]]; then
         ANTIGEN_CHECK_FILES+=("${${funcfiletrace[6]%:*}##* }")
       fi
